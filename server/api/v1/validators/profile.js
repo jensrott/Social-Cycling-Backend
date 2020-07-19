a8ac98597137264f14150a23d950d4bbc312111b
jensrott <jensrott@student.arteveldehs.be>
2020-07-19 23:19:04 +0200

    Change

--- server/api/v1/validators/profile.js
+++ server/api/v1/validators/profile.js
@@ -2,26 +2,46 @@ const Validator = require("validator");
 const isEmpty = require("./isEmpty");
 
 module.exports = function validateProfileInput(data) {
+  let errors = {};
 
-    let errors = {};
+  data.username = !isEmpty(data.username) ? data.username : "";
+  data.level = !isEmpty(data.level) ? data.level : "";
+  data.social.youtube = !isEmpty(data.social.youtube)
+    ? data.social.youtube
+    : "";
+  data.social.instagram = !isEmpty(data.social.instagram)
+    ? data.social.instagram
+    : "";
+  data.social.twitter = !isEmpty(data.social.twitter)
+    ? data.social.twitter
+    : "";
 
-    data.username = !isEmpty(data.username) ? data.username : "";
-    data.level = !isEmpty(data.level) ? data.level : "";
+  if (Validator.isEmpty(data.username)) {
+    errors.username = "Username field is required";
+  }
 
-    if (Validator.isEmpty(data.username)) {
-        errors.username = "Username field is required";
-    }
+  if (Validator.isEmail(data.username)) {
+    errors.username = "Username cannot be an email";
+  }
 
-    if (Validator.isEmail(data.username)) {
-        errors.username = "Username cannot be an email";
-    }
+  if (Validator.isEmpty(data.level)) {
+    errors.level = "Skill level field is required";
+  }
 
-    if (Validator.isEmpty(data.level)) {
-        errors.level = "Skill level field is required";
-    }
+  if (Validator.isEmail(data.social.youtube)) {
+    errors.youtube = "Youtube cannot be an email";
+  }
 
-    return {
-        errors,
-        isValid: isEmpty(errors)
-    };
+  if (Validator.isEmail(data.social.instagram)) {
+    errors.instagram = "Instagram cannot be an email";
+  }
+
+  if (Validator.isEmail(data.social.twitter)) {
+    errors.twitter = "Twitter cannot be an email";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
 };
